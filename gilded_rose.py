c307341e4c20281c56ef5053abe5daa2d20dacd3
jshuang2 <jeffshuang2@gmail.com>
2025-02-12 22:46:02 -0800

    Refactor code for lab 5. All tests pass and source code is easier to read and is more maintainable.

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 class Item:
     """ DO NOT CHANGE THIS CLASS!!!"""
     def __init__(self, name, sell_in, quality):
@@ -12,38 +11,98 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
-class GildedRose(object):
+class LegendaryItem(Item):
 
+    def update(self):
+        self.update_sell_in()
+
+    def update_sell_in(self):
+        self.sell_in -= 1
+
+
+class GildedItem(Item):
+
+    def update(self):
+        self.update_sell_in()
+        self.update_quality()
+
+    def update_sell_in(self):
+        self.sell_in -= 1
+
+    def update_quality(self):
+        if self.sell_in < 0:
+            self.quality -= 2
+        else:
+            self.quality -= 1
+
+        if self.quality < 0:
+            self.quality = 0
+
+
+class NormalItem(GildedItem):
+    pass
+
+
+class AgedBrie(GildedItem):
+
+    def update_quality(self):
+        if self.sell_in < 0:
+            self.quality += 2
+        else:
+            self.quality += 1
+
+        if self.quality > 50:
+            self.quality = 50
+
+
+class BackstagePass(GildedItem):
+
+    def update_quality(self):
+        if self.sell_in < 0:
+            self.quality = 0
+        elif self.sell_in < 5:
+            self.quality += 3
+            if self.quality > 50:
+                self.quality = 50
+        elif self.sell_in < 10:
+            self.quality += 2
+            if self.quality > 50:
+                self.quality = 50
+        else:
+            self.quality += 1
+            if self.quality > 50:
+                self.quality = 50
+
+
+class Sulfuras(LegendaryItem):
+
+    def update_quality(self):
+        pass
+
+
+class Conjured(GildedItem):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+        self.isConjured = True
+
+    def update_quality(self):
+        if self.sell_in < 0:
+            self.quality -= 4
+        else:
+            self.quality -= 2
+
+        if self.quality < 0:
+            self.quality = 0
+
+
+class GildedRose:
     def __init__(self, items: list[Item]):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            item.update()
+
+    def get_items(self):
+        return [item.name for item in self.items]
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,54 +1,54 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import Item, GildedRose, Conjured, Sulfuras
 
 
 class GildedRoseTest(unittest.TestCase):
 
     # example of test that checks for logical errors
     def test_sulfuras_should_not_decrease_quality(self):
-        items = [Item("Sulfuras", 5, 80)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
+        items = [Sulfuras("Sulfuras", 5, 80)]
         sulfuras_item = items[0]
+        sulfuras_item.update()
         self.assertEqual(80, sulfuras_item.quality)
         self.assertEqual(4, sulfuras_item.sell_in)
         self.assertEqual("Sulfuras", sulfuras_item.name)
 
     # example of test that checks for syntax errors
     def test_gilded_rose_list_all_items(self):
-        items = [Item("Sulfuras", 5, 80)]
+        items = [Sulfuras("Sulfuras", 5, 80)]
         gilded_rose = GildedRose(items)
         all_items = gilded_rose.get_items()
         self.assertEqual(["Sulfuras"], all_items)
 
     # test that sulfuras should decrease sell_in (logical)
     def test_sulfuras_should_decrease_sell_in(self):
-        items = [Item("Sulfuras, Hand of Ragnaros", 5, 50)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
+        items = [Sulfuras("Sulfuras, Hand of Ragnaros", 5, 50)]
         sulfuras_item = items[0]
+        sulfuras_item.update()
         self.assertEqual(4, sulfuras_item.sell_in)
 
     # item quality should never be more than 50 (logical)
     def test_item_quality_not_over_50(self):
-        items = [Item("item", 30, 80)]
+        quality = 50
+        items = [Item("item", 30, quality)]
         item = items[0]
         maxQuality = 50
         self.assertGreaterEqual(maxQuality, item.quality)
 
     # conjured item should degrade twice as fast (logical)
     def test_conjured_item_should_degrade_quality_twice_as_fast(self):
-        items = [Item("Conjured Item", 5, 80)]
+        quality = 80
+        items = [Conjured("Conjured Item", 5, quality)]
         conjured_item = items[0]
-        conjured_item.update_quality()
+        conjured_item.update()
         normal_degradation_rate = 1
-        self.assertEqual(normal_degradation_rate * 2, conjured_item.quality)
+        self.assertEqual(quality - (normal_degradation_rate * 2), conjured_item.quality)
 
     # conjured item does not exist (syntax error)
     def test_conjured_item(self):
-        items = [Item("Conjured Item", 5, 80)]
+        items = [Conjured("Conjured Item", 5, 80)]
         conjured_item = items[0]
         self.assertEqual(True, conjured_item.isConjured)
 
